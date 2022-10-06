45350ba6fd0cc4a1b4b97f4e5f05d5a094ef9bb8
Haruka <hfujii1013@gmail.com>
2022-10-06 14:53:38 -0700

    done

---
+++ Lesson06_01.java
@@ -0,0 +1,17 @@
+class Lesson06_01 {
+    public static void main(String[] args){
+
+    
+        int var;
+        var = 1;
+        int var01 = 2;
+        // int var-01 = 3;
+        // int 01var = 4;
+
+        System.out.println(var);
+        System.out.println(var01);
+        // System.out.println(var_01);
+        // System.out.println(var$01);
+
+    }
+}
---
+++ Lesson10_01.java
@@ -0,0 +1,13 @@
+public class Lesson10_01 {
+    public static void main(String[] args){
+        int age = 0;
+
+        if (age >= 20) {
+            System.out.println("adult");
+        } else if (age==0) {
+            System.out.print("baby");
+        } else {
+            System.out.print("child");
+        }
+    }
+}
